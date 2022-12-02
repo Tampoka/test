0455c3aa16d601cd2cb2c5e5f1b9cce678359d82
Olga-dot-art <116544878+Olga-dot-art@users.noreply.github.com>
2022-12-02 21:01:13 +0900

    Fix arguments type.

--- src/hooks/useFetching.ts
+++ src/hooks/useFetching.ts
@@ -1,10 +1,12 @@
 import {useState} from 'react';
+import {Optional} from '../types/shared-types';
 
-export const useFetching = (callback: { <T>(...args: T[]) : Promise<void> }): [{ <T>(...args: T[]): Promise<void> }, boolean, string] => {
+
+export const useFetching = (callback: { (...args: Optional<number | string>[]): Promise<void> }): [{ (...args: Optional<number | string>[]): Promise<void> }, boolean, string] => {
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState('')
 
-    const fetching: {<T>(...args:T[]): Promise<void> } = async<T> (...args: T[]):Promise<void> => {
+    const fetching = async (...args: Optional<number | string>[]): Promise<void> => {
         try {
             setIsLoading(true)
             await callback(...args)
